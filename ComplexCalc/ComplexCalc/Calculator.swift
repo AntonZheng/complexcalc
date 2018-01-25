fdbff20b5293529da9b9f051346454301c559ed2
antonzheng <anton.zheng@gmail.com>
2018-01-24 16:51:51 -0800

    finished all except last add and subtract func

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -16,14 +16,22 @@ class Calculator {
     
     func add(_ args: [Int]) -> Int{
         var result = 0
-        for i in 0...args.count {
+        for i in 0...(args.count-1) {
             result += args[i]
         }
         return result
     }
     
+    func add(lhs: (Int,Int), rhs: (Int,Int)) -> (Int,Int){
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+    
     func subtract(lhs: Int, rhs: Int) -> Int {
         return lhs - rhs
+    }
+    
+    func subtract(lhs: (Int,Int), rhs: (Int,Int)) -> (Int,Int){
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
     }
     
     func multiply(lhs: Int, rhs: Int) -> Int {
@@ -32,7 +40,7 @@ class Calculator {
     
     func multiply(_ args: [Int]) -> Int{
         var result = 0
-        for i in 0...args.count {
+        for i in 0...(args.count-1) {
             result *= args[i]
         }
         return result
@@ -44,7 +52,7 @@ class Calculator {
     
     func count(_ args: [Int]) -> Int{
         var result = 0
-        for _ in 0...args.count {
+        for _ in 0...(args.count-1) {
             result += 1
         }
         return result
@@ -58,5 +66,11 @@ class Calculator {
         return op(lhs,rhs)
     }
     
-    func mathOps(
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        var result = beg
+        for i in 0...(args.count-1) {
+            result = op(result,args[i])
+        }
+        return result
+    }
 }
